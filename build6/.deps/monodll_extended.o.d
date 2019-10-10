980e27ebfe01843359090043be44ef5939b61d4b
igavronski <github@proxima.adm.br>
2019-10-10 16:00:06 -0300

    The config that worked on my Android 9 phone is in build6

---
+++ build6/.deps/monodll_extended.o.d
@@ -0,0 +1,8 @@
+monodll_extended.o: ../src/common/extended.c ../include/wx/defs.h \
+  ../include/wx/platform.h ../include/wx/android/config_android.h \
+  ../include/wx/compiler.h \
+  /data/data/com.termux/files/home/wxWidgets/build6/lib/wx/include/gtk2-unicode-3.1/wx/setup.h \
+  ../include/wx/chkconf.h ../include/wx/gtk/chkconf.h \
+  ../include/wx/unix/chkconf.h ../include/wx/version.h \
+  ../include/wx/cpp.h ../include/wx/dlimpexp.h ../include/wx/types.h \
+  ../include/wx/features.h ../include/wx/math.h
